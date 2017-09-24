807a0b706852db6ac98defe83129e9e572ff754e
Zhong Chu <Zhong.Chu16@student.xjtlu.edu.cn>
2017-09-24 17:30:28 +0800

    Add files via upload

--- C Practice/9.2 color2.cpp
+++ C Practice/9.2 color2.cpp
@@ -1,9 +1,34 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#define MAXLINE 1000   /*maximum input line size*/
+
+int getline1(char line[],int maxline);
+int remove2(char s[]);
+
+/*remove trailing blanks and tabs, and delete blank lines*/
 int main()
 {
-	system("color f5");
-	printf("wa wa wa");
-	system("pause");
+	char line[MAXLINE];  /*current input line*/
+	
+	while(getline1(line,MAXLINE)>0)
+		if(remove2(line)>0)
+			printf("%s",line);
 	return 0;
+}
+int remove2(char s[])
+{
+	int i;
+	
+	i=0;
+	while(s[i]!='\n') /*fine newline character */
+		++i;
+	--i;
+	while(i>= 0 && (s[i] == ' ' || s[i] == '\t'))
+		--i;
+	if(i>= 0 ){
+		++i;
+		s[i] = '\n';
+		++i;
+		s[i] = '\0';		
+	}
+	return i;
 }
