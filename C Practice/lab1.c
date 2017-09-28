584a426d68daa90271d8c4bd7b5c7e218930561a
Zhong Chu <Zhong.Chu16@student.xjtlu.edu.cn>
2017-09-28 14:59:04 +0800

    Add files via upload

---
+++ C Practice/lab1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	/*int x,y,z;
+		/*x=(12+6)/2*3;
+		y=x=(2+3)/4;*/
+		/*y=3+2*(x=7/2);*/
+		/*x=(int)3.8+3.3;
+		y=22.0*(int)3/10;*/
+		/*y=22.0*(int)(3/10);*/
+		/*y=(2+3)*10.5;
+		printf("%d",y);*/
+	int a=1,b=1;
+	int aplus,plusb;
+	aplus=a++;
+	plusb=++b;
+	printf("a aplus plusb b\n");
+	printf("%1d\t%5d\t%5d\t%5d\n",a,aplus,plusb,b);
+}3/2
