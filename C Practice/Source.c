9f7972f03e111ef56a83ba014dd71d66e1541849
Zhong Chu <Zhong.Chu16@student.xjtlu.edu.cn>
2017-10-11 12:57:31 +0800

    Add files via upload

---
+++ C Practice/Source.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+	int i = 7, j = 2;
+	if (i == 1)
+	if (j == 2)
+		printf("%d\n", i = i + j);
+	printf("%d\n", i = i - j);
+	printf("%d\n", i);
+//	system("pause");
+}
